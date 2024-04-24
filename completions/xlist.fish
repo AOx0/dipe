2e71e899405c360a053a2f820d7f250831a974e9
Alejandro Osornio <aoxo.contact@gmail.com>
2024-04-24 02:48:00 -0600

    Add xlist select

--- completions/xlist.fish
+++ completions/xlist.fish
@@ -1,10 +1,10 @@
-set -l commands sheets headers uniques help
-set -l actions sheets headers uniques
+set -l commands sheets headers uniques select help
+set -l actions sheets headers uniques select
 
 function __fish_xlist_contains_sheets
     set -l cmd_args (commandline -opc)
     set -l contains n
-    set -l list_arr headers uniques
+    set -l list_arr headers uniques select
 
     for arg in $cmd_args
         if contains -- $arg $list_arr
@@ -34,7 +34,7 @@ function __fish_xlist_complete_headers
 end
 
 function __fish_xlist_help_subcommand_completion
-    set -l commands sheets headers uniques help
+    set -l commands sheets headers uniques select help
     set -l cmd_args (commandline -opc)
 
     if test (count $cmd_args) -eq 2
@@ -50,6 +50,7 @@ complete -c xlist -n "not __fish_seen_subcommand_from $commands" -a sheets -d "L
 complete -c xlist -n "not __fish_seen_subcommand_from $commands" -a headers -d "List available headers in a sheet from the specified path"
 complete -c xlist -n "not __fish_seen_subcommand_from $commands" -a help -d "Print help of the given subcommand(s)"
 complete -c xlist -n "not __fish_seen_subcommand_from $commands" -a uniques -d "Print unique values for the fiven columns from a sheet in a path"
+complete -c xlist -n "not __fish_seen_subcommand_from $commands" -a select -d "Print all values for the given columns"
 complete -c xlist -n "__fish_seen_subcommand_from $actions; and __fish_is_nth_token 2" -F
 
 # xlist headers
@@ -58,6 +59,10 @@ complete -c xlist -n "__fish_seen_subcommand_from headers; and not __fish_seen_s
 # xlist uniques
 complete -c xlist -n "__fish_seen_subcommand_from uniques; and not __fish_seen_subcommand_from help; and __fish_is_nth_token 3" -ka '(__fish_xlist_complete_sheets)'
 complete -c xlist -n "__fish_seen_subcommand_from uniques; and not __fish_seen_subcommand_from help; and not __fish_is_nth_token 3; and not __fish_is_nth_token 2" -ka '(__fish_xlist_complete_headers)'
+
+# xlist select
+complete -c xlist -n "__fish_seen_subcommand_from select; and not __fish_seen_subcommand_from help; and __fish_is_nth_token 3" -ka '(__fish_xlist_complete_sheets)'
+complete -c xlist -n "__fish_seen_subcommand_from select; and not __fish_seen_subcommand_from help; and not __fish_is_nth_token 3; and not __fish_is_nth_token 2" -ka '(__fish_xlist_complete_headers)'
 
 
 # xlist help
--- utils/src/bin/xlist.rs
+++ utils/src/bin/xlist.rs
@@ -37,6 +37,17 @@ enum Command {
         #[clap(short, long)]
         save: Option<PathBuf>,
     },
+    Select {
+        /// The path of the xlsx-like file
+        path: PathBuf,
+        /// The name of the sheet
+        sheet: String,
+        /// The headers to select
+        headers: Vec<String>,
+        /// Path to save the result. The file extension matters
+        #[clap(short, long)]
+        save: Option<PathBuf>,
+    },
     #[clap(hide = true)]
     CompleteSheets { path: PathBuf },
     #[clap(hide = true)]
@@ -256,6 +267,124 @@ fn main() {
                         let mut workook = Workbook::new();
                         let mut worksheet = Worksheet::new();
                         worksheet.set_name("Uniques").unwrap();
+
+                        for (row, values) in vfinal.iter().enumerate() {
+                            for (col, value) in values.iter().enumerate() {
+                                worksheet
+                                    .write(row.try_into().unwrap(), col.try_into().unwrap(), value)
+                                    .unwrap();
+                            }
+                        }
+
+                        workook.push_worksheet(worksheet);
+                        workook.save(save_path).unwrap();
+                    }
+                    _ => {
+                        eprintln!("Invalid extension {ext:#?}");
+                    }
+                },
+                None => {
+                    eprintln!("Error: El archivo no tiene extensión")
+                }
+            }
+        }
+        Command::Select {
+            path,
+            sheet,
+            headers,
+            save,
+        } => {
+            let mut sheets = open_workbook_auto(path).unwrap();
+            let Ok(sheet) = sheets.worksheet_range(&sheet) else {
+                eprintln!("Hubo un problema abriendo {sheet:?}, seguro que existe?");
+                return;
+            };
+
+            let mut rows = sheet.rows().filter(|row| row.iter().any(|a| !a.is_empty()));
+            // .filter(|row| row.first().is_some_and(|c| c.is_empty().not()));
+
+            let Some(available_headers) = rows.next() else {
+                eprintln!("No hay encabezado en la hoja {sheet:?}");
+                return;
+            };
+
+            let available_headers = available_headers
+                .iter()
+                .map(|v| match v {
+                    calamine::Data::String(v) => v.to_owned(),
+                    v => v.to_string(),
+                })
+                .collect::<Vec<_>>();
+
+            let mut finals = Vec::new();
+
+            for (n_col, header) in headers.iter().enumerate() {
+                let Some(col) = available_headers.iter().position(|h| h == header) else {
+                    eprintln!("Error: Can not find header {header:?}, skipping.");
+                    continue;
+                };
+
+                rows.clone()
+                    .map(|row| &row[col])
+                    .map(|v| match v {
+                        calamine::Data::String(v) => v.to_string(),
+                        v => v.to_string(),
+                    })
+                    .enumerate()
+                    .for_each(|(row, v)| {
+                        let row = if let Some(v) = finals.get_mut(row) {
+                            v
+                        } else {
+                            let len = finals.len();
+                            finals.reserve(row - finals.len());
+                            for _ in len..row + 1 {
+                                finals.push(vec![String::new(); headers.len()]);
+                            }
+                            finals.get_mut(row).unwrap()
+                        };
+
+                        row[n_col] = v;
+                    })
+            }
+
+            let mut vfinal = Vec::new();
+            let n_head = headers.len();
+            vfinal.push(headers);
+            vfinal.extend(finals.into_iter().collect::<Vec<Vec<_>>>());
+
+            let Some(save_path) = save else {
+                for r in vfinal {
+                    for (i, c) in r.iter().enumerate() {
+                        print!("{c:?}{}", if i + 1 < n_head { "," } else { "" });
+                    }
+                    print!("\n");
+                }
+                return;
+            };
+
+            match save_path
+                .as_os_str()
+                .as_encoded_bytes()
+                .rsplit_once_str(".")
+            {
+                Some((_, ext)) => match ext {
+                    b"csv" => {
+                        let mut file = std::fs::File::create(save_path).unwrap();
+                        for vec in vfinal {
+                            for (i, v) in vec.iter().enumerate() {
+                                if i < vec.len() - 1 {
+                                    write!(file, "{v:#?},").unwrap();
+                                } else {
+                                    write!(file, "{v:#?}").unwrap();
+                                }
+                            }
+                            writeln!(file).unwrap();
+                        }
+                    }
+                    b"xlsx" => {
+                        let mut workook = Workbook::new();
+                        let mut worksheet = Worksheet::new();
+                        worksheet.set_name("Values").unwrap();
 
                         for (row, values) in vfinal.iter().enumerate() {
                             for (col, value) in values.iter().enumerate() {
